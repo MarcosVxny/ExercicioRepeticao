eb8ba2715a04fd6e3088c2259a1854d7acb476dd
MarcosVxny <Oliveira.marcos1280@gmail.com>
2023-02-15 23:33:11 -0300

    ExercicioRepetiçao

---
+++ Exercicio.java
@@ -0,0 +1,29 @@
+package ExercíciosdeRepetição;
+
+import java.util.Scanner;
+
+public class Exercicio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x,div;
+		
+		for(x=1000; x<=1999; x++) {
+			
+			if(x%11==5) {
+				System.out.print("\n"+x);
+				
+				
+				
+			}
+			
+			
+		}
+		
+		
+		
+		
+	}
+
+}
